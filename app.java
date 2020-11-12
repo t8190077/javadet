48c7f006d88741249b9c21f9b9c54e433c84a73e
Eric Tzimas <errikos.tzimas@gmail.com>
2020-11-12 18:46:59 +0200

    Erics commit

--- app.java
+++ app.java
@@ -1,6 +1,7 @@
 //JavaDet Hello World
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+        System.out.println("Eric is here, test"); 
     }
 }
