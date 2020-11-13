73d8dfa555a3a3ff949b5e4d10008d8835c55e8b
Eric Tzimas <errikos.tzimas@gmail.com>
2020-11-13 04:45:58 +0200

    Aight guys. Download these files, compile them with javac and run the Main.java.I made a small interface for the terminal. Also created main class and Patient class. Patient class created patients. Saves them to arrays. Interface has some extra usages. --READ COMMENTS IN CODE.

--- Main.java
+++ Main.java
@@ -1,9 +1,32 @@
-public class Main extends Patients{
+// Here is the main class. From here all the classes of our program will be called.
+// importing scanner to get input.
+import java.util.Scanner;
+public class Main {
     public static void main(String[] args) {
+    // Setting name to null basically and id = 0 for the first person.
+    String name = "";
+    int id = 0;
+    // I created a while loop, this loop always asks for a new input, a new patient. It stops only when you input 'quit'.
+    while (!name.equals("quit")) {
         System.out.println("Enter patients name. ");
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        Patients adam = new Patients(name);
-        adam.getPatient();
-    };
+        name = sc.nextLine();
+    // if you input '/all' it shows you all the names that have already given it.
+        if (name.equals("/all")) {
+            Patients.allNames();
+            continue;
+        }
+        if (name.equals("quit")) {
+            break;
+        }
+    // Creating new patient object..
+        Patients adam = new Patients(name, id);
+    // Informing the user on the new information of the patient.
+        System.out.println(adam.getPatient());
+        // Automatically counting a new ID for each patient.
+        id ++;
+
+    }
+      
+    }
 }
