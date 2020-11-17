dcef508bbadb53462ee3afd8f2e178bbf970230d
Michalis Kouzelis <mikekouzgr@gmail.com>
2020-11-17 13:21:50 +0200

    Create Patients as 2d array

--- Patients.java
+++ Patients.java
@@ -1,35 +1,59 @@
 import java.util.ArrayList;
+// Patient class that uses 2D array to add patients
+// amount of rows is dynamic and changes with ArrayList functions
+// but amount of column (which represents the Patients attributes is fixed) so it enters
+// a String array[5] in every ArrayList cell
 public class Patients {
-    // Initialising name and id.
-    String name;
-    int id;
-    // These are two arraylists, in which we are going to input our names and ids.
-    static ArrayList<Integer> ids = new ArrayList<>();
-    static ArrayList<String> names = new ArrayList<>();
-    // Patient object constructor.
-    public Patients(String name, int id) {
-        this.name = name;
-        this.id = id;
-    // Adding patients attributes to lists.
-        ids.add(id); 
-        names.add(name);
-    }
-    // Simple toString to return name.
-    public String toString() {
-        return name;
-    }
-    // Simple toString to return id.
-    public int getId() {
-        return id;
-    }
-    // Custom toString that return full id + name.
-    public String getPatient() {
-        return "NAME: " + name + "\nID: " + id;
-    }
-    // Function that returns all patients names in a nice format.
-    public static void allNames() {
-        for (int i = 0; i <= names.size() - 1; i++) {
-            System.out.println("ID: " + i + " NAME: " + names.get(i));
-        }
-    }
-} 
+	// Initializing attributes.
+	String name, amka, dob, email, number; // dob = date of birth
+	// Initializing ArrayList with String[] array in every cell
+	static ArrayList<String[]> patients = new ArrayList<String[]>();
+	// Patient object constructor with attributes as arguments.
+	public Patients(String name, String amka, String dob, String email, String number) {
+		String[] data = new String[5];
+		this.name = name;
+		this.amka = amka;
+		this.dob = dob;
+		this.email = email;
+		this.number = number;
+		// enter patient's data to a String[5] array that later stores in patients ArrayList
+		data[0] = name;
+		data[1] = amka;
+		data[2] = dob;
+		data[3] = email;
+		data[4] = number;
+		// stores String[] data to ArrayList<String[]> patients.
+		patients.add(data);
+		}
+	//custom toString that returns String format with current Employee's data
+	@Override
+	public String toString() {
+		return "Name: " + this.name + "\nAmka: " + this.amka + "\nDate of Birth: " + this.dob + "\nEmail: " + this.email + "\nPhone number: " + this.number;
+	}
+	// Method that returns name.
+	public String getName() {
+		return name;
+	}
+	
+	// Method that prints data from all patients
+	public static void allPatients() {
+		System.out.print("Patients: ");
+		// loop for every ArrayList row
+		for (String[] array : patients) {
+			System.out.println();
+			// loop for every ArrayList column
+			for (String element : array) {
+				System.out.printf("%s  ", element);
+			}
+		}
+	}
+	// Method that prints all patient's names only
+	public static void getNames() {
+		for (int i=0; i<patients.size(); i++) {
+			System.out.println(patients.get(i)[0]);
+		}
+	}
+}
+	
+
+
