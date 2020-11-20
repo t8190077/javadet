d30a98ba211899584ea97b2a4eafff43b96b4c8c
Michalis Kouzelis <mikekouzgr@gmail.com>
2020-11-20 13:07:28 +0200

    Add other attributes to patient

--- Main.java
+++ Main.java
@@ -3,14 +3,18 @@ import java.io.File;  // Importing the File class
 import java.io.IOException;  // Importing the IOException class to handle err
 import java.io.FileWriter;   // Importing the FileWriter class
 import java.util.ArrayList; // Importing the ArrayList class
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 // Here is the main class. From here all the classes of our program will be called.
 public class Main {
     static ArrayList<Patient> pats = new ArrayList<>();
 
     public static void main(String[] args) {
-    // Setting name to null basically and id = 0 for the first person.
-    String name = "";
-    
+    // Setting name and attributes to null basically and id = 0 for the first person.
+    String phonenumber, amka, dob, email, name, input = "";
+    Date dobform = null;
     
     // Creating new file if it doesnt exist, reading data from existing file.
     try {
@@ -28,10 +32,19 @@ public class Main {
       Patient.readdata();
       int id = Patient.returnNames().size();
     // I created a while loop, this loop always asks for a new input, a new patient. It stops only when you input 'quit'.
-    while (!name.equals("quit")) {
+      do {
         System.out.println("Enter patients name. ");
         Scanner sc = new Scanner(System.in);
         name = sc.nextLine();
+        // Enters additional information
+        System.out.println("Enter patients amka: ");
+        amka = sc.nextLine();
+        System.out.println("Enter patients date of birth: ");
+        dob = sc.nextLine();
+        System.out.println("Enter patients email: ");
+        email = sc.nextLine();
+        System.out.println("Enter patients phone number: ");
+        phonenumber = sc.nextLine();
     // if you input '/all' it shows you all the names that have already given it.
         if (name.equals("/all")) {
             Patient.allNames();
@@ -48,24 +61,40 @@ public class Main {
             break;
         }
     // Creating new patient object..
-        Patient adam = new Patient(name, id);
+        Patient adam = new Patient(name, id, amka, dob, email, phonenumber);
         pats.add(adam);
     // Informing the user on the new information of the patient.
         System.out.println(adam.getPatient());
         // Automatically counting a new ID for each patient.
         id ++;
-    // Writing results to .txt file.
-    }
+ 
+        System.out.println("Press quit if you want to exit. ");
+        input = sc.nextLine();
+    } while (!input.equals("quit")); 
+   // Writing results to .txt file.
     try {
         FileWriter myWriter = new FileWriter("data.txt");
         ArrayList<String> data = new ArrayList<>();
         ArrayList<Integer> data2 = new ArrayList<>();
-
+        // New addition ----- Adding write for other attributes as well----------- 
+        ArrayList<String> data3 = new ArrayList<>(); // data3 is for amka.
+        ArrayList<String> data4 = new ArrayList<>(); // data 4 is for date of birth.
+        ArrayList<String> data5 = new ArrayList<>(); // data 5 is for email.
+        ArrayList<String> data6 = new ArrayList<>(); // data 4 is for phone number.
+         
         data = Patient.returnNames();
         data2 = Patient.returnIds();
+        data3 = Patient.returnAmkas();
+        data4 = Patient.returnDobs();
+        data5 = Patient.returnEmails();
+        data6 = Patient.returnPhoneNumbers();
         for(int i = 0; i <= data.size() - 1; i++) {
             myWriter.write(data.get(i) + ",");
             myWriter.write(data2.get(i) + "\n");
+            myWriter.write(data3.get(i) + "\n");
+            myWriter.write(data4.get(i) + "\n");
+            myWriter.write(data5.get(i) + "\n");
+            myWriter.write(data6.get(i) + "\n");
         }
         myWriter.close();
         System.out.println("Successfully wrote to the file.");
@@ -73,6 +102,8 @@ public class Main {
         System.out.println("An error occurred.");
         e.printStackTrace();
       }
-      
+    
+   
     }
+    
 }
--- Patient.java
+++ Patient.java
@@ -3,9 +3,11 @@ import java.io.File;  // Importing the File class
 import java.io.FileNotFoundException;  // Importing this class to handle errors
 import java.io.IOException;  // Importing the IOException class to handle err
 import java.util.Scanner; // Importing the Scanner class to read text files
+import java.util.Date;
 public class Patient {
     // Initialising name and id.
-    String name;
+    String name, amka, email, phonenumber;
+	String dob;
     static int id;
     // Creating method that reads data from file.
     public static void readdata() {
@@ -15,7 +17,7 @@ public class Patient {
             while (myReader.hasNextLine()) {
               String data = myReader.nextLine();
               String[] myArray = data.split(",");
-              Patient adam = new Patient(myArray[0],Integer.parseInt(myArray[1])); 
+              Patient adam = new Patient(myArray[0],Integer.parseInt(myArray[1]), myArray[2], myArray[3], myArray[4], myArray[5]);  //TODO
               id = ids.size();
               Main.pats.add(adam);             
             }
@@ -29,15 +31,27 @@ public class Patient {
     static ArrayList<Integer> ids = new ArrayList<>();
     static ArrayList<String> names = new ArrayList<>();
     static ArrayList<String> datab = new ArrayList<>();
+    static ArrayList<String> amkas = new ArrayList<>();
+    static ArrayList<String> emails = new ArrayList<>();
+    static ArrayList<String> phonenumbers = new ArrayList<>();
+    static ArrayList<String> dobs = new ArrayList<>();
 
 
     // Patient object constructor.
-    public Patient(String name, int id) {
+    public Patient(String name, int id, String amka, String dob, String email, String phonenumber) {
         this.name = name;
         this.id = id;
+        this.amka = amka;
+        this.dob = dob;
+        this.email = email;
+        this.phonenumber = phonenumber;
     // Adding patients attributes to lists.
         ids.add(id); 
         names.add(name);
+        amkas.add(amka);
+        dobs.add(dob);
+        emails.add(email);
+        phonenumbers.add(phonenumber);
     }
     // Simple toString to return name.
     public String toString() {
@@ -51,10 +65,23 @@ public class Patient {
     public String getPatient() {
         return "NAME: " + name + "\nID: " + id;
     }
+    //Custom toString that returns all patient's attributes
+    public String getPatientData() {
+    	return "NAME: " + name + "\nID: " + id + "\nAmka: " + amka
+    			+ "\nDate of Birth: " + dob + "\nEmail: " + email
+    			+ "\nPhone Number: " + phonenumber;
+    }
     // Function that returns all patients names in a nice format.
     public static void allNames() {
         for (int i = 0; i <= names.size() - 1; i++) {
             System.out.println("ID: " + i + " NAME: " + names.get(i));
+        }
+    }
+    // Method  that returns all patients data in a nice format.
+    public static void allData() {
+        for (int i = 0; i <= names.size() - 1; i++) {
+            System.out.println("ID: " + i + " NAME: " + names.get(i) + "\nAmka: " + amkas.get(i) + " Date of Birth: " + dobs.get(i)
+            + "\nEmail: " + emails.get(i) + " Phone Number: " + phonenumbers.get(i));
         }
     }
     // Static method that returns arraylist of names.
@@ -65,6 +92,22 @@ public class Patient {
      public static ArrayList<Integer> returnIds() {
         return ids;
     }
+     // Static method that returns arraylist of amkas.
+     public static ArrayList<String> returnAmkas() {
+     	return amkas;
+     }
+     // Static method that returns arraylist of date of births.
+     public static ArrayList<String> returnDobs() {
+      	return dobs;
+     }
+     // Static method that returns arraylist of emails.
+     public static ArrayList<String> returnEmails() {
+      	return emails;
+     }
+     // Static method that returns arraylist of phone numbers.
+     public static ArrayList<String> returnPhoneNumbers() {
+      	return phonenumbers;
+     }
       // Function that returns a string array of all patient names in a nice format,used for GUI
       public static String [] namesArr() {
         String arr [] = new String[names.size()];
